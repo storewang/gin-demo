46fd0f0a946ac41aa55bdbe9e3c0f76b23e487f3
shitou <shitoumgzf.com>
2022-09-05 11:25:17 +0800

    添加优雅关闭

--- web/server.go
+++ web/server.go
@@ -1,8 +1,14 @@
 package web
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -25,30 +31,31 @@ func NewServer() *Server {
 
 func (s *Server) Run() {
 	fmt.Println("listen ", s.config.Server.Host)
-	s.server.Run(s.config.Server.Host)
-	// srv := &http.Server{
-	// 	Addr:    s.config.Server.Host,
-	// 	Handler: s.server,
-	// }
+	//s.server.Run(s.config.Server.Host)
+	srv := &http.Server{
+		Addr:    s.config.Server.Host,
+		Handler: s.server,
+	}
 
-	// go func() {
-	// 	// 服务连接
-	// 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-	// 		log.Fatalf("listen: %s\n", err)
-	// 	}
-	// }()
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-	// // 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
-	// quit := make(chan os.Signal)
-	// signal.Notify(quit, os.Interrupt)
-	// <-quit
-	// log.Println("Shutdown Server ...")
-	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// defer cancel()
-	// if err := srv.Shutdown(ctx); err != nil {
-	// 	log.Fatal("Server Shutdown:", err)
-	// }
-	// log.Println("Server exiting")
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// 在此阻塞
+	<-quit
+	log.Println("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting...")
 }
 
 func (s *Server) init() {
